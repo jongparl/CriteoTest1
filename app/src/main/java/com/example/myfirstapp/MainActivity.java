b4e320b85e4b932a67576802bdc934d7559d9e03
j.park <j.park@criteo.com>
2020-08-07 11:24:02 +0900

    change comments

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -9,5 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // This is testing app for Criteo SDK integration
+        // Test Test
+
     }
 }
